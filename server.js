2b421128e69d54640f3436d2c0926f1b49d7a0b7
Mediogre <caseyarrington13@gmail.com>
2021-04-21 20:33:22 -0500

    add id-parem route

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ function filterByQuery (query, animalsArray) {
     return filteredResults
 }
 
+function findById(id, animalsArray) {
+    //const result = animalsArray.find(animal => animal.id === id)
+
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    
+    return result;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -58,6 +65,16 @@ app.get('/api/animals', (req, res) => {
     }
     // send back {animals} as a json
     res.json(results);
+})
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 })
 
 app.listen(PORT, () => {
